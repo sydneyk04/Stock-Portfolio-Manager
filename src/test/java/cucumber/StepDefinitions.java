b1a20bee61d01726e6fc92c432e433591fa89369
Nandhakumar Saravana <nsaravan@usc.edu>
2020-09-21 15:55:06 -0700

    Added acceptance tests for Login page

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class StepDefinitions {
 	private static final String ROOT_URL = "http://localhost:8080/";
+	private static final String Login_URL = "http://localhost:8080/login.jsp";
 
 	private final WebDriver driver = new ChromeDriver();
 
@@ -38,7 +39,109 @@ public class StepDefinitions {
 	public void i_should_see_text(String text) {
 		assertTrue(driver.getPageSource().contains(text));
 	}
+	
+	@Given("I am on the login page")
+	public void i_am_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		driver.get(Login_URL);
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("I enter my username")
+	public void i_enter_my_username() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		String usr = "johnDoe";
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[2]/input")).sendKeys(usr);
+		
+		
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	@When("I enter my password")
+	public void i_enter_my_password() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		String usr = "test123";
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[3]/input")).sendKeys(usr);
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	@When("I click the login button")
+	public void i_click_the_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		driver.findElement(By.xpath("//*[@id=\"login-form-submit\"]")).click();
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	@Then("I should be on the dashboard page")
+	public void i_should_be_on_the_dashboard_page() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.getCurrentUrl().equalsIgnoreCase("http://localhost:8080/home.jsp");
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
 
+	@When("I enter an incorrect password")
+	public void i_enter_an_incorrect_password() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		String usr = "test1234";
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[3]/input")).sendKeys(usr);
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("I should the error message {string}")
+	public void i_should_the_error_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String info = driver.switchTo().alert().getText();
+		
+		//String info = driver.findElement(By.id("login-error-msg")).getText();
+		
+		assertTrue(info.equalsIgnoreCase(string));
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("I should the alert {string}")
+	public void i_should_the_alert(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String info = driver.switchTo().alert().getText();
+		
+		assertTrue(info.equalsIgnoreCase(string));
+		
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	
 	@After()
 	public void after() {
 		driver.quit();
