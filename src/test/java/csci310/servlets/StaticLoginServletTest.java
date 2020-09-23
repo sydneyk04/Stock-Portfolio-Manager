6c90a95864796fb031214874468f4ffce7eb71f3
macuser <macuser@macs-mbp>
2020-09-22 22:12:12 -0700

    Add more junit test cases

---
+++ src/test/java/csci310/servlets/StaticLoginServletTest.java
@@ -0,0 +1,108 @@
+package csci310.servlets;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+
+import com.google.api.core.ApiService.Listener;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(JUnit4.class)
+@PrepareForTest({ FirebaseDatabase.class, FirebaseOptions.class} )
+public class StaticLoginServletTest extends Mockito{
+ 
+ @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpServletResponse response;
+    
+    @Mock
+    HttpSession session;
+    
+    @Mock
+    RequestDispatcher rd;
+    
+    LoginServlet servlet;
+    
+    @Mock
+    DatabaseReference mockedDatabaseReference;
+    
+    @Before
+    public void setUp() throws Exception {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        session = mock(HttpSession.class);
+        rd = mock(RequestDispatcher.class);
+        servlet = new LoginServlet();
+         
+        when(request.getSession()).thenReturn(session); 
+        
+        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
+        
+        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
+        
+        PowerMockito.mockStatic(FirebaseDatabase.class);
+   
+        when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
+    }  
+    
+    
+    @Test
+    public void testLoginOnCancelled() throws IOException {
+     
+     when(mockedDatabaseReference.child(anyString())).thenReturn(mockedDatabaseReference);
+     
+     StringWriter writer = new StringWriter();
+     PrintWriter out = new PrintWriter(writer);
+     
+     when(response.getWriter()).thenReturn(out);
+     
+     doAnswer(new Answer<Void>() {
+    	 public Void answer(InvocationOnMock invocation) throws Throwable {
+    		 ValueEventListener valueEventListener = (ValueEventListener) invocation.getArguments()[0];
+    		 DatabaseError error = mock(DatabaseError.class);
+    		 valueEventListener.onCancelled(error);
+    		 return null;     
+    	 }
+     }).when(mockedDatabaseReference).addListenerForSingleValueEvent(any(ValueEventListener.class));
+     
+     doThrow(IOException.class)
+  		.when(response)
+  		.sendRedirect(anyString());
+          
+     servlet.doPost(request, response);
+    }
+    
+}
