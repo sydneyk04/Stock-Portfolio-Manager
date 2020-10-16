10cc3c21673ff9f7ee42cf20c57b2f4f2c4d8728
Sarah Cheung <cheungsa@usc.edu>
2020-10-16 01:32:07 -0700

    updated dashboardservlet unit test

--- src/test/java/csci310/servlets/DashboardServletTest.java
+++ src/test/java/csci310/servlets/DashboardServletTest.java
@@ -14,6 +14,8 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import csci310.Portfolio;
+
 public class DashboardServletTest extends Mockito {
 	@Mock
 	HttpServletRequest request;
@@ -27,11 +29,25 @@ public class DashboardServletTest extends Mockito {
 	@Mock
 	RequestDispatcher rd;
 	
+	@Mock
+	Portfolio p;
+	
 	DashboardServlet servlet;
 
 	@Before
 	public void setUp() throws Exception {
-		servlet = new DashboardServlet();
+		request = Mockito.mock(HttpServletRequest.class);
+    	response = Mockito.mock(HttpServletResponse.class);
+    	session = Mockito.mock(HttpSession.class);
+    	rd = Mockito.mock(RequestDispatcher.class);
+    	p = Mockito.mock(Portfolio.class);
+    	servlet = new DashboardServlet();
+		
+		session.setAttribute("username", "johnDoe");
+		
+		when(request.getSession()).thenReturn(session);
+		when(request.getSession().getAttribute("username")).thenReturn("johnDoe");
+		when(request.getParameter("username")).thenReturn("johnDoe");
 	}
 
 	@Test
