77a85398648965f53bc3a356f3100cc5c568fece
Sarah Cheung <cheungsa@usc.edu>
2020-10-29 15:08:23 -0700

    acceptance tests for app compatibility

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -8,9 +8,11 @@ import static org.junit.Assert.assertTrue;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -536,6 +538,7 @@ public class StepDefinitions {
 		WebElement button = driver.findElement(By.id("reportrange"));
 		button.click();
 	}
+	
 	@Given("I select an appropriate date range")
 	public void i_select_an_appropriate_date_range() {
 		WebElement button = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/table/tbody/tr[3]/td[3]"));
@@ -543,42 +546,60 @@ public class StepDefinitions {
 		button = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[1]/table/tbody/tr[5]/td[3]"));
 		button.click();
 	}
+	
 	@Given("I try to enter an invalid date")
 	public void i_try_to_enter_an_invalid_date() {
 		WebElement button = driver.findElement(By.id("reportrange"));
 		//this might have to change depending on how Paul sets up input
 		button.sendKeys("09/09/2019 - 09/09/2020");
 	}
+	
 	@Given("I enter an appropriate date range")
 	public void i_enter_an_appropriate_date_range() {
 		WebElement button = driver.findElement(By.id("reportrange"));
 		//this might have to change depending on how Paul sets up input
 		button.sendKeys("01/01/2020 - 09/09/2020");
 	}
+	
 	@Given("I click the button to add the S&P")
 	public void i_click_the_button_to_add_the_S_P() {
 	    WebElement button = driver.findElement(By.id("spytoggle"));
 	    button.click();
 	}
+	
 	@Given("I click the button to remove the S&P")
 	public void i_click_the_button_to_remove_the_S_P() {
 	    WebElement button = driver.findElement(By.id("spytoggle"));
 	    button.click();
 	}
+	
 	@Then("I should see the graph date range change")
 	public void i_should_see_the_graph_date_range_change() {
 		assertTrue(true);
 	}
+	
 	@Then("I should see the S&P stock removed from the graph")
 	public void i_should_see_the_S_P_stock_removed_from_the_graph() {
 		assertTrue(true);
 	}
+	
 	@Then("I should see the S&P stock added to the graph")
 	public void i_should_see_the_S_P_stock_added_to_the_graph() {
 		assertTrue(true);
 	}
-
 	
+	/**************************
+	 * APP COMPATIBILITY FEATURE
+	 **************************/
+	@When("I resize the window to {int}%")
+	public void i_resize_the_window_to(Integer int1) {
+		Dimension size = driver.manage().window().getSize();
+		int height = size.getHeight();
+		int width = size.getWidth();
+		driver.manage().window().setPosition(new Point(0, 0));
+		driver.manage().window().setSize(new Dimension(width/2, height/2));
+	}
+
 	@After()
 	public void after() {
 		driver.quit();
