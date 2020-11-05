540b531e7c2d30622e45836f4b23a3977e606bd2
Hao Sun <sun715@usc.edu>
2020-11-04 20:57:13 -0800

    Fixed ui for stock list

--- src/main/webapp/production/js/csv/importCSV.js
+++ src/main/webapp/production/js/csv/importCSV.js
@@ -37,6 +37,8 @@ $(document).ready(function() {
                     shares: lineElements[5]
                   }
                   var username = lineElements[0];
+                  var buyDate = newData["from"];
+                  var sellDate = newData["to"];
                   console.log(username);
                   // Update existing node if ticker/from/to match, else add new one
                   var flagToAppendHTML = true;
@@ -44,25 +46,20 @@ $(document).ready(function() {
                   ref.once("value")
                     .then(function(snapshot) {
                     if(snapshot.val() == null) {
-                      ref.update(newData);
-                      console.log("updated");
                     } else if(snapshot.val()["from"] == newData["from"] && snapshot.val()["to"] == newData["to"]) {
                       newData["shares"] = (parseFloat(newData["shares"]) + parseFloat(snapshot.val()["shares"])).toString();
-                      ref.update(newData);
-                      document.querySelector("#li-"+ticker).getElementsByTagName('p')[5].innerHTML = newData["shares"];
+                      document.querySelector("#li-"+ticker).getElementsByTagName('p')[3].innerHTML = newData["shares"];
                       flagToAppendHTML = false;
                     } else {
-                      ref.update(newData);
-                      console.log("updated");
                     }
                   }).then(() => {
                     if(flagToAppendHTML) {
                       var appendingHTML = `
-                      <li id="li-`+ ticker +`">
+                      <li id="li-`+ ticker +`" class="d-flex">
                           <div style="display:inline; float: left; width: 15%;">
-                            <button type="button" style="background:lightgrey; border:none; border-radius:5px; color:white;" class="flat" data-toggle="modal" data-target="#removeStockModal`+ticker+shares+buyDate+sellDate+`">X</button><br>
+                            <button type="button" style="background:lightgrey; border:none; border-radius:5px; color:white;" class="flat" data-toggle="modal" data-target="#removeStockModal-`+ticker+`">X</button><br>
                             <!-- Modal for Remove Stock -->
-                            <div class="modal fade" id="removeStockModal`+ticker+shares+buyDate+sellDate+`" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="removeStockModal-`+ticker+`" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                               <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                   <div class="modal-header">
@@ -73,7 +70,11 @@ $(document).ready(function() {
                                   </div>
                                   <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                    <button type="button" class="btn btn-primary deletestock" data-dismiss="modal" id="stockremovebutton">Remove Stock</button>
+                                    <form class="" action="/dashboard" method="post">
+                                      <input type="hidden" name="action" value="removeStock">
+                                      <input type="hidden" name="ticker" value="<%=myStocks.get(i).get(0) %>">
+                                      <button type="submit" class="btn btn-primary deletestock" data-dismiss="modal" id="stockremovebutton">Remove Stock</button>
+                                    </form>
                                   </div>
                                 </div>
                               </div>
@@ -81,17 +82,30 @@ $(document).ready(function() {
                           </div>
                           <div style="float: left; width: 85%;">
                             <div style="display:inline;">
-                              <p style="text-align:left;display:inline;">   Exchange: </p><p style="text-align:left; display:inline; font-weight:bold;">`+ name + `</p>
-                              <br>
-                            </div>
-                            <div style="display:inline;">
                               <p style="text-align:left;display:inline;">   Ticker: </p><p style="text-align:left; display:inline; font-weight:bold;">`+ ticker + `</p>
                               <br>
                             </div>
                             <div style="display:inline;">
-                              <p style="text-align:left;display:inline;">   # Shares: </p><p style="text-align:left; display:inline; font-weight:bold;">${newData["shares"]}</p>
+                              <p style="text-align:left;display:inline;">   # Shares: </p><p style="text-align:left; display:inline; font-weight:bold;">`+ lineElements[5] + `</p>
                               <br>
                             </div>
+                            <div style="display:inline;">
+                              <p style="text-align:left;display:inline;">   Purchase Date: </p><p style="text-align:left; display:inline; font-weight:bold;">${newData["from"]}</p>
+                              <br>
+                            </div>
+                            <div style="display:inline;">
+                              <p style="text-align:left;display:inline;">   Sell Date: </p><p style="text-align:left; display:inline; font-weight:bold;">${newData["to"]}</p>
+                              <br>
+                            </div>
+                            <div style="display:inline;">
+                             <p style="text-align:left;display:inline;">   Calculate in Portfolio: </p><p style="text-align:left; display:inline; font-weight:bold;">
+                               <form name="formname" action="/dashboard" method="POST">
+                                 <input type="hidden" name="action" value="portfolioState">
+                                 <input type="hidden" name="ticker" value="${ticker}">
+                                 <button style="text-align:left;display:inline;" type="submit"class="btn btn-light btn-sm">Yes</button>
+                               </form>
+                             </p>
+                           </div>
                         </div>
                         <br>
                         <br>
